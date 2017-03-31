7818b717d9e0043cf367a6187e97ba03e83ceb11
gnuheike@gmail.com <gnuheike@gmail.com>
2017-04-01 00:41:20 +0600

    phhishcehck

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 "use strict";
-require('strict-mode')(function () {
-    const Discord = require("discord.js");
-    const client = new Discord.Client();
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
-    client.on('message', msg => {
-        var text = '**' + msg.member.displayName + '** [*' + msg.guild.name + '*]: \n' + msg.content;
-        client.guilds.forEach(function (guild) {
-            if (client.user.id !== msg.author.id && guild.id !== msg.guild.id && msg.channel.name == 'general') {
-                if (guild.id == 297387654931152896) {
-                    guild.channels.find('name', 'general').sendMessage(text);
-                }
+client.on('message', msg => {
+    var text = '**' + msg.member.displayName + '** [*' + msg.guild.name + '*]: \n' + msg.content;
+    client.guilds.forEach(function (guild) {
+        if (client.user.id !== msg.author.id && guild.id !== msg.guild.id && msg.channel.name == 'general') {
+            if (guild.id == 297387654931152896) {
+                guild.channels.find('name', 'general').sendMessage(text);
             }
-        });
+        }
     });
-
-    client.login('Mjk3MzkyNTUyODA0MzUyMDAx.C8AH7g.MwcZ4xpQsejOYQn4nKE0QlfTzpc');
 });
+
+client.login('Mjk3MzkyNTUyODA0MzUyMDAx.C8AH7g.MwcZ4xpQsejOYQn4nKE0QlfTzpc');
