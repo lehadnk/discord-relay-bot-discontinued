007c2e6524fed52bc38d6112de949cc206587c57
gnuheike@gmail.com <gnuheike@gmail.com>
2017-04-01 02:23:50 +0600

    phhishcehck

--- index.js
+++ index.js
@@ -3,17 +3,28 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 var http = require('http');
-http.createServer(function(req, res){}).listen(process.env.PORT || 6000);
+http.createServer(function (req, res) {}).listen(process.env.PORT || 6000);
 
 client.on('message', msg => {
     var text = '**' + msg.member.displayName + '** [*' + msg.guild.name + '*]: \n' + msg.content;
+    var embed = {};
+    if (typeof msg.attachments.first() !== 'undefined') {
+        embed = {
+            'image': {
+                'url': msg.attachments.first().url
+            }
+        };
+    }
+   
     client.guilds.forEach(function (guild) {
         if (client.user.id !== msg.author.id && guild.id !== msg.guild.id && msg.channel.name == 'general') {
             if (guild.id == 297387654931152896) {
-                guild.channels.find('name', 'general').sendMessage(text);
+                guild.channels.find('name', 'general').sendMessage(text, {
+                    'embed': embed
+                });
             }
         }
     });
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN || 'Mjk3MzkyNTUyODA0MzUyMDAx.C8AH7g.MwcZ4xpQsejOYQn4nKE0QlfTzpc');
