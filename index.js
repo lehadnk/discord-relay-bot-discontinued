c7230926706d9d6a82ac38cf75fc7cf5cd897a4b
Alexey Zauzin <lehadnk@gmail.com>
2017-04-02 02:30:43 +0300

    Black lists

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const client = new Discord.Client();
 const http = require('http');
 http.createServer(function (req, res) {}).listen(process.env.PORT || 6000);
 
+// Loading blacklist
+var fs = require('fs');
+var blacklist = fs.readFileSync('blacklist.txt').toString().split("\n");
+
 var stringToColour = function (str) {
     var hash = 0;
     for (var i = 0; i < str.length; i++) {
@@ -17,20 +21,46 @@ var stringToColour = function (str) {
     return colour;
 }
 
-client.on('message', msg => {
-    var embed = new Discord.RichEmbed()
-            .setAuthor(msg.member.displayName + '  [' + msg.guild.name + ']', msg.member.user.displayAvatarURL)
-            .setDescription(msg.content)
-            .setColor(stringToColour(msg.member.displayName));
-    if (typeof msg.attachments.first() !== 'undefined') {
-        embed.setImage(msg.attachments.first().url);
-    }
-
-    client.guilds.forEach(function (guild) {
-        if (client.user.id !== msg.author.id && msg.author.bot == false && guild.id !== msg.guild.id && msg.channel.name == 'general') {
-            guild.channels.find('name', 'general').sendEmbed(embed);
+var getInfo = function (name, channel) {
+    client.users.forEach(function (user) {
+        if (user.username == name) {
+            channel.send(user.username+': '+user.id);
         }
     });
+}
+
+var ban = function (id, channel) {
+    blacklist.push(id);
+    fs.appendFile('blacklist.txt', id);
+    channel.send(id+' was added to relay blacklist.');
+}
+
+client.on('message', msg => {
+    if (msg.content.match(/^\/info ((?! ).)*$/)) {
+    var params = msg.content.split(' ');
+    getInfo(params[1], msg.channel);
+    return;
+}
+
+if (msg.content.match(/^\/ban ((?! ).)*$/)) {
+    var params = msg.content.split(' ');
+    ban(params[1], msg.channel);
+    return;
+}
+
+var embed = new Discord.RichEmbed()
+    .setAuthor(msg.member.displayName + '  [' + msg.guild.name + ']', msg.member.user.displayAvatarURL)
+    .setDescription(msg.content)
+    .setColor(stringToColour(msg.member.displayName));
+if (typeof msg.attachments.first() !== 'undefined') {
+    embed.setImage(msg.attachments.first().url);
+}
+
+client.guilds.forEach(function (guild) {
+    if (client.user.id !== msg.author.id && msg.author.bot == false && guild.id !== msg.guild.id && msg.channel.name == 'general' && (blacklist.indexOf(msg.author.id) == -1)) {
+        guild.channels.find('name', 'general').sendEmbed(embed);
+    }
+});
 });
 
 client.login(process.env.BOT_TOKEN);
