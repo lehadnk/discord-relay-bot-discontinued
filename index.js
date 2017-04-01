be55fbe76f29e759f1a1d7741958e8a228d016ec
gnuheike@gmail.com <gnuheike@gmail.com>
2017-04-01 12:33:18 +0600

    text message to embed

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 "use strict";
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
-var http = require('http');
+const http = require('http');
 http.createServer(function (req, res) {}).listen(process.env.PORT || 6000);
 
-client.on('message', msg => {
-    var text = '**' + msg.member.displayName + '** [*' + msg.guild.name + '*]: \n' + msg.content;
-    var embed = {};
-    if (typeof msg.attachments.first() !== 'undefined') {
-        embed = {
-            'image': {
-                'url': msg.attachments.first().url
-            }
-        };
+var stringToColour = function (str) {
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
     }
-   
+    var colour = '#';
+    for (var i = 0; i < 3; i++) {
+        var value = (hash >> (i * 8)) & 0xFF;
+        colour += ('00' + value.toString(16)).substr(-2);
+    }
+    return colour;
+}
+
+client.on('message', msg => {
+    var embed = new Discord.RichEmbed()
+            .setAuthor(msg.member.displayName + '  [' + msg.guild.name + ']', msg.member.user.displayAvatarURL)
+            .setDescription(msg.content)
+            .setColor(stringToColour(msg.member.displayName));    
+    if (typeof msg.attachments.first() !== 'undefined') {
+        embed.setImage(msg.attachments.first().url);
+    }
+
     client.guilds.forEach(function (guild) {
-        if (client.user.id !== msg.author.id && guild.id !== msg.guild.id && msg.channel.name == 'general') {
+        if (client.user.id !== msg.author.id && msg.author.bot == false && guild.id !== msg.guild.id && msg.channel.name == 'general') {
             if (guild.id == 297387654931152896) {
-                guild.channels.find('name', 'general').sendMessage(text, {
-                    'embed': embed
-                });
+                guild.channels.find('name', 'general').sendEmbed(embed);
             }
         }
     });
