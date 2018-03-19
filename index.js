bf9a8dd364042a27db6c92566271e59df1448370
Alexey Zauzin <lehadnk@gmail.com>
2018-03-19 17:08:36 +0300

    Added some classes

--- index.js
+++ index.js
@@ -59,25 +59,33 @@ var getNickname = function(msg) {
 
 var getColor = function(msg) {
     switch(msg.guild.id) {
-        case '207912188407578624':
+        case '207912188407578624': // priest
             return '#FFFFFF';
-        case '215548192891076610':
+        case '215548192891076610': // dh
             return '#A330C9';
-        case '217529023838814208':
+        case '217529023838814208': // rogue
             return '#FFF569';
-        case '217529109272592384':
+        case '217529109272592384': // dk
             return '#C41F3B';
-        case '203632333620772874':
+        case '203632333620772874': // druid
             return '#FF7D0A';
-        case '215427955193544704':
+        case '215427955193544704': // hunter
             return '#ABD473';
-        case '210643527472906241':
+        case '210643527472906241': // paladin
             return '#F58CBA';
-        case '214750173413376003':
+        case '214750173413376003': // shaman
             return '#0070DE';
+        case '212664465181769728': // mage
+            return '#69CCF0';
+        case '217529170291458048': // warlock
+            return '#9482C9';
+        case '217528830418616322': // warrior
+            return '#C79C6E';
+        case '217529277489479681': // monk
+            return '#00FF96';
     }
     
-    return '#999999';
+    return '#999999'; // undefined
 }
 
 client.on('message', msg => {
