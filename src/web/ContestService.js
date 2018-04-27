ca224344a10708bcb35b8c5996d3251f712240d8
Alexey Zauzin <lehadnk@gmail.com>
2018-04-27 19:14:52 +0300

    Shuffling leaders

--- src/web/ContestService.js
+++ src/web/ContestService.js
@@ -10,19 +10,46 @@ class ContestService {
             "GROUP BY p.id\n" +
             "ORDER BY votes DESC",
             (err, rows) => {
-                callback(rows);
+                let result = [];
+                rows.forEach((r) => {
+                    result.push({
+                        id: r.id,
+                        name: r.name,
+                        image_url: r.image_url
+                    });
+                });
+
+                function shuffle(a) {
+                    var j, x, i;
+                    for (i = a.length - 1; i > 0; i--) {
+                        j = Math.floor(Math.random() * (i + 1));
+                        x = a[i];
+                        a[i] = a[j];
+                        a[j] = x;
+                    }
+                    return a;
+                }
+
+                let leaders = result.splice(0, 5);
+                let leadersShuffled = shuffle(leaders);
+
+                leadersShuffled.forEach((l) => {
+                    result.unshift(l);
+                });
+
+                callback(result);
             });
     }
 
-    getParticipant(participantId, callback) {
-        this.db.all("SELECT *\n" +
-            "FROM votes\n" +
-            "WHERE participant_id = ?1",
-            {1: participantId},
-            (err, rows) => {
-                callback(rows);
-            });
-    }
+    // getParticipant(participantId, callback) {
+    //     this.db.all("SELECT *\n" +
+    //         "FROM votes\n" +
+    //         "WHERE participant_id = ?1",
+    //         {1: participantId},
+    //         (err, rows) => {
+    //             callback(rows);
+    //         });
+    // }
 
     getVotersList(callback) {
         this.db.all("SELECT discord_id, discord_name, count(id) as votes\n" +
